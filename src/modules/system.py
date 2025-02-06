787069b22357683e6710f1c6878d2503dbcb86c7
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2025-02-07 01:15:16 +0400

    Полностью переписан класс System:\n- методы получения системной информации (в большинстве своём) больше не используют команды стандартной оболочки Linux;\n- добавлены докстринги ко всем методам

--- src/modules/system.py
+++ src/modules/system.py
@@ -1,102 +1,153 @@
 import logging
-import subprocess
-import time
+import os
+import sys
+from collections import namedtuple
+from platform import system
+from shutil import disk_usage
+from time import sleep
+
+ntuple_memusage = namedtuple("MemUsage", "total used free")
 
 
 class System:
+    """
+    Класс для получения системной информации,
+    такой как использование CPU, памяти,
+    диска и размер файлов/директорий.
 
-    _logs_owner: str = __qualname__
+    .. note::
+        Класс работает только на Linux-системах.
+        При попытке использования на других ОС
+        программа завершится с кодом 1.
+    """
 
-    @classmethod
-    def run_cmd(cls, command: str):
-        return subprocess.run(
-            args = command,
-            stdout = subprocess.PIPE,
-            stderr = subprocess.PIPE,
-            shell = True,
-            text = True
-            )
+    if system() != "Linux":
+        sys.exit(1)
 
     @classmethod
     def _get_cpu_time(cls):
+        """
+        Внутренний метод для получения данных о времени работы CPU
+        из `/proc/stat`.
+
+        :return: Кортеж из двух значений: время простоя (idle)
+        и общее время работы CPU (total).
+        :rtype: tuple[int, int]
+        """
         with open("/proc/stat", "r") as file:
             line = file.readline()
             parts = line.split()
-            idle_time = int(parts[4])
-            total_time = sum(map(int, parts[1:8]))
-        return idle_time, total_time
+            idle = int(parts[4])
+            total = sum(map(int, parts[1:8]))
+        return idle, total
 
     @classmethod
-    def get_cpu_usage(cls, interval: float = 0.1):
+    def get_cpu_usage(cls):
+        """
+        Возвращает текущую загрузку CPU в процентах.
+
+        :return: Загрузка CPU в процентах (от 0 до 100).
+        :rtype: float
+        :raises Exception: Если не удалось получить данные о загрузке CPU.
+        """
         try:
-            if not isinstance(interval, (int, float)):
-                raise TypeError("задан некорректный тип интервала")
-            else:
-                if interval <= 0:
-                    raise ValueError("интервал для расчётов загрузки ЦП не может быть <= 0")
-                elif interval > 1:
-                    raise ValueError("слишком большой интервал для расчётов загрузки ЦП")
             idle, total = cls._get_cpu_time()
-            time.sleep(interval)
+            sleep(0.1)
             idle2, total2 = cls._get_cpu_time()
-            cpu_usage = (1 - (idle2 - idle) / (total2 - total)) * 100
-            return cpu_usage
+            return (1 - (idle2 - idle) / (total2 - total)) * 100
         except Exception as err:
-            msg = "не удалось получить данные об использовании носителя"
-            logging.error(f'{System._logs_owner}: {msg}: {err}')
-            return
+            msg = "не удалось получить данные о загрузке процессора"
+            logging.warning(f"{cls.__name__}: {msg}: {err}")
 
     @classmethod
-    def get_ram_usage(cls):
+    def get_mem_usage(cls):
+        """
+        Возвращает информацию об использовании оперативной памяти.
+
+        :return: Именованный кортеж с полями:
+            - `total`: Общий объем памяти (в байтах).
+            - `used`: Используемый объем памяти (в байтах).
+            - `free`: Свободный объем памяти (в байтах).
+        :rtype: ntuple_memusage
+        :raises Exception: Если не удалось получить данные об использовании памяти.
+        """
         try:
             with open("/proc/meminfo", "r") as file:
-                lines = file.readlines()
-            mem_total = int(lines[0].split()[1])
-            mem_free = int(lines[1].split()[1])
-            buff_cache = int(lines[3].split()[1]) + int(lines[4].split()[1])
-            mem_used = mem_total - mem_free - buff_cache
-            memory_used_percent = (mem_used / mem_total) * 100
-            return memory_used_percent
+                lines = tuple(
+                    map(
+                        lambda x: int(x.split()[1]) * 2**10,
+                        file.readlines()[:5],
+                    )
+                )
+            total, free = lines[:2]
+            buff_cache = sum(lines[3:5])
+            used = total - free - buff_cache
+            return ntuple_memusage(total, used, total - used)
         except Exception as err:
             msg = "не удалось получить данные об использовании оперативной памяти"
-            logging.error(f'{System._logs_owner}: {msg}: {err}')
-            return
+            logging.warning(f"{cls.__name__}: {msg}: {err}")
 
     @classmethod
-    def get_diskspace_usage(cls, detail: bool = False):
-        _format = lambda param: f"{param / 1024:.2f}"
-        cmd = r"sudo df -B1 /boot | awk '{print $3,$2}' | grep -E '^[^А-Яа-я]+$'"
+    def get_disk_usage(cls):
+        """
+        Возвращает информацию об использовании дискового пространства.
+
+        :return: Именованный кортеж с полями:
+            - `total`: Общий объем дискового пространства (в байтах).
+            - `used`: Используемый объем дискового пространства (в байтах).
+            - `free`: Свободный объем дискового пространства (в байтах).
+        :rtype: ntuple_memusage
+        :raises Exception: Если не удалось получить данные об использовании диска.
+        """
         try:
-            shell = System.run_cmd(cmd)
-            if not shell.stdout or shell.stderr:
-                raise SystemError(
-                    f"ошибка выполнения команды {cmd!r}: " + shell.stderr.strip()
-                )
-            _int = lambda raw: int(raw.rstrip("%"))
-            used, total = \
-                map(_int, shell.stdout.split())
-            usage_percent = used / total * 100
-            if not detail:
-                return round(usage_percent, 2)
-            else:
-                return total, used, round(usage_percent, 2)
+            return ntuple_memusage(*disk_usage("/"))
         except Exception as err:
-            msg = "не удалось получить данные об использовании носителя"
-            logging.error(f'{System._logs_owner}: {msg}: {err}')
-            return
+            msg = "не удалось получить данные об использовании дискового пространства"
+            logging.warning(f"{cls.__name__}: {msg}: {err}")
 
     @classmethod
-    def get_dir_size(cls, filepath: str):
+    def get_file_size(cls, filepath: str):
+        """
+        Возвращает размер файла в байтах.
+
+        :param filepath: Путь к файлу.
+        :type filepath: str
+        :return: Размер файла в байтах.
+        :rtype: int
+        :raises FileNotFoundError: Если файл не существует.
+        :raises Exception: Если не удалось получить данные о размере файла.
+        """
         try:
-            cmd = f"sudo du -sh -B1 {filepath} " + r"| awk '{print $1}'"
-            shell = System.run_cmd(cmd)
-            if not shell.stdout or shell.stderr:
-                raise SystemError(
-                    f"ошибка выполнения команды {cmd!r}: " + shell.stderr.strip()
-                )
-            value = shell.stdout.strip()
-            return int(value)
+            if not os.path.exists(filepath):
+                raise FileNotFoundError(f"пути {filepath!r} не существует")
+            return os.path.getsize(filepath)
+        except Exception as err:
+            msg = "не удалось получить данные о размере файла"
+            logging.warning(f"{cls.__name__}: {msg}: {err}")
+
+    @classmethod
+    def get_dir_size(cls, path: str):
+        """
+        Возвращает общий размер директории в байтах.
+
+        :param path: Путь к директории.
+        :type path: str
+        :return: Общий размер директории в байтах.
+        :rtype: int
+        :raises SystemError: Если директория не существует.
+        :raises Exception: Если не удалось получить данные о размере директории.
+        """
+        try:
+            if not os.path.exists(path):
+                raise SystemError(f"пути {path!r} не существует")
+            total_size = 0
+            for dirpath, _, filenames in os.walk(path):
+                for filename in filenames:
+                    filepath = os.path.join(dirpath, filename)
+                    size = cls.get_file_size(filepath)
+                    if size:
+                        total_size += size
+            return total_size
         except Exception as err:
             msg = "не удалось получить данные о размере директории"
-            logging.error(f'{System._logs_owner}: {msg}: {err}')
-            return
+            logging.warning(f"{cls.__name__}: {msg}: {err}")
