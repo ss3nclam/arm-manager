ad3596f322dab97c9454db03f52343c1771db5af
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2025-02-23 18:25:22 +0400

    Удалён test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# import re
-
-
-# reg = r"\x1b\[[0-9;]*[mGKH]"
-# x = "\x1b[1;31m0.39/17.5 GB\x1b[0m"
-# print(re.findall(reg, x))
-
-for timestamp in (i * 3_600 for i in (24, 12, 6, 3, 1)):
-    print(timestamp)
